9d81ccf8801a855870795885c227a0aa3c2d9906
Mike Duke <aithon@gmail.com>
2018-10-02 20:58:39 -0600

    Pies, clubs, cakes, mods, and bosses prompts complete

--- prototypes/index.js
+++ prototypes/index.js
@@ -16,11 +16,21 @@ const turingPrompts = {
     //  { name: 'Robbie', studentCount: 18 }
     // ]
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = instructors.map((instructor) => {
+      let students = cohorts.find((cohort) => {
+        return cohort.module === instructor.module;
+      })
+      return {name: instructor.name, studentCount: students.studentCount};
+    })
     return result;
-
+    
     // Annotation:
-    // Write your annotation here as a comment
+    // Dataset: 2 arrays
+    //  return: an array
+    //  method: map
+    //  dataset to iterate over: instructors
+    //  in common: module
+    //  
   },
 
   studentsPerInstructor() {
@@ -98,12 +108,17 @@ const modPrompts = {
     //   { mod: 3, studentsPerInstructor: 10 },
     //   { mod: 4, studentsPerInstructor: 8 }
     // ]
-
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = mods.map((currentMod) => {
+      let studentsPer = currentMod.students / currentMod.instructors;
+      return {mod: currentMod.mod, studentsPerInstructor: studentsPer}
+    }) 
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // dataset: a single array
+    // return: a single array of the same length
+    // method: map
+    // since we are starting with a single array of mods and returning an array of the same length, i used map to iterate over the mods array. From the callback, i returned an object with a key of mod assugned to the value of the current mod number and another key of students per instructor, which was assigned to the number of the students in the mod divided by the number of instructors. 
   }
 };
 
@@ -190,11 +205,30 @@ const cakePrompts = {
     // every cake in the dataset e.g.
     // ['dutch process cocoa', 'toasted sugar', 'smoked sea salt', 'berries', ..etc]
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    // iterate over the cakes array, then iterate over the toppings array. 
+    // start by flattening all toppings arrays together?
+    // then iterate over the new array and check for duplicates? 
+
+    const result = cakes.reduce((toppingsArray, cake) => {
+      var cakeTopping = cake.toppings;
+      cakeTopping.forEach((topping) => {
+        if (!toppingsArray.includes(topping)) {
+          toppingsArray.push(topping)
+        }
+      })
+      return toppingsArray;
+    }, [])
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // dataset: one array
+    // return: array of different size and content
+    // method: reduce
+    // inner loop: 
+    //    dataset: toppings array for each cake
+    //    return: a single topping that does not match the others in the array (could push into an empty array)
+    //    method: forEach
+    // since we are starting from one large array and trying to create a new array that does not match the size or overall subset of the original array, i chose to use a reduce. Once i started working on the reduce i realized i would also need to iterate over the array of toppings and do something with each one of them. Since i had the empty array for the accumulator, i knew i could push into that array once i was iterating over the individual toppings arrays, thus a forEach was fine. 
   },
 
   groceryList() {
@@ -208,11 +242,26 @@ const cakePrompts = {
     //    ...etc
     // }
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const allToppings = cakes.reduce((toppingsArray, cake) => {
+      toppingsArray = toppingsArray.concat(cake.toppings)
+      return toppingsArray;
+    }, [])
+    
+    const result = allToppings.reduce((list, topping) => {
+      if (list[topping] > 0) {
+        list[topping]++
+      } else {
+        list[topping] = 1;
+      }
+      return list
+    }, {})
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // dataset: single array
+    // return: object
+    // method: reduce
+    // since we are working with the single cakes array and we want to return an object, i used reduce. I started by iterating over the cakes array and contatingating each toppings array for each cake into a single array using reduce. Then, i used reduce on the resulting array and had it set the key of the accumulator object to the name of the topping and count how many of each topping were in the array. 
   },
 
   stockPerCake() {
@@ -224,25 +273,42 @@ const cakePrompts = {
     //    ..etc
     // ]
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = cakes.map((cake) => {
+      return {flavor: cake.cakeFlavor, inStock: cake.inStock}
+    })
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // dataset: single array 
+    // return: array of the same length
+    // method: map
+    // since we start with a single array and want to return an array of the same length, i used map. i returned an object that contained keys as were given and values accessed by dot notation for each individual cake.
   },
 
   totalInventory() {
-    // Return the total amout of cakes in stock e.g.
+    // Return the total amount of cakes in stock e.g.
     // 59
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = cakes.reduce((total, cake) => {
+      total = total + cake.inStock;
+      return total;
+    }, 0)
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // dataset: single array
+    // return: a single number
+    // method: reduce
+    // since we are working with a single array and returning a single number, i used reduce. i then reassigned the accumulator to the accumulator itself (starting at 0) to the number of cakes in stock for each cake
   },
 
   onlyInStock() {
+    const result = cakes.filter((cake) => {
+      return cake.inStock;
+    })
+
+    return result;
+
     // Return an array of only the cakes that are in stock
     // e.g.
     // [
@@ -262,6 +328,9 @@ const cakePrompts = {
     // },
     // ..etc
     // ]
+
+    // Annotation:
+    // I'm recieving an array of cakes and i want a subset of that array, so i'm using filter. The filter callback will return only the cakes that have an inStock value;
   }
 };
 
@@ -292,12 +361,16 @@ const piePrompts = {
     //   cinnamon: 50,
     //   sugar: 100
     // }
-
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    let piesRemaining = pie.desiredInventoryCount - pie.inventoryCount;
+    let cinnamonRemaining = pie.ingredients.cinnamon * piesRemaining; 
+    let sugarRemaining = pie.ingredients.sugar * piesRemaining;
+    const result = {cinnamon: cinnamonRemaining, sugar: sugarRemaining};
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // dataset: single object
+    // return: single object
+    // since we are starting with an object, i used dot notation to access the properties i wanted--some of them properties within properties--and put those values into a new returned object. 
   }
 };
 
@@ -327,11 +400,32 @@ const clubPrompts = {
     //   ...etc
     // }
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const namesArray = clubs.reduce((namesArray, club) => {
+      return namesArray.concat(club.members)
+    }, [])
+    const noDuplicateNames = namesArray.reduce((nameKey, name) => {
+      if (!nameKey.includes(name)) {
+        nameKey.push(name);
+      }
+      return nameKey;
+    }, [])
+    const result = noDuplicateNames.reduce((obj, nameKey) => {
+      obj[nameKey] = [];
+      clubs.forEach((club) => {
+        if (club.members.includes(nameKey)) {
+          obj[nameKey].push(club.club);
+        }
+      })
+      return obj
+    }, {})
     return result;
 
     // Annotation:
     // Write your annotation here as a comment
+    // dataset: single array
+    // return: object with keys of member names and values of arrays that represent the clubs that member is in. 
+    // method: reduce
+    // since we are working with a single array, but we were looking to create an object with the names of the club members as keys, i started by iterating over the array of clubs and accessing the members of each club object using dot notation to create a single array with all of the member names. i used a reduce on this new array in order to remove any duplicate names. finally, i iterated over this array without duplicate names using a reduce since we ulitmately wanted to return an object. i created a key in the empty object for each name and set the value to an empty array. i then iterated over the clubs array again inside of the reduce in order to check each club's members and see if they matched the key name. if they did, i pushed the name of the club into the empty array.  
   }
 };
 
@@ -361,11 +455,27 @@ const bossPrompts = {
     //   { bossName: 'Scar', sidekickLoyalty: 16 }
     // ]
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    var bossKeys = Object.keys(bosses);
+    var bossNames = bossKeys.map((boss) => {
+      return bosses[boss].name;
+    })
+  
+    var result = bossNames.map((boss) => {
+      let bossObj = {bossName: boss, sidekickLoyalty: 0}
+      sidekicks.forEach((sidekick) => {
+        if (sidekick.boss === bossObj.bossName) {
+          bossObj.sidekickLoyalty += sidekick.loyaltyToBoss;
+        }
+      })
+      return bossObj;
+    })
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // dataset: an object of bosses with properties that are also objects, and an array of sidekicks with objects as elements
+    // return: an array of objects with the name of the boss and their total loyalty
+    // method: Object.keys to get the boss names in an array; iterate over the boss names array and compare the boss name to the boss of the sidekick and if they match, assign their loyalty to the object in the returned array. 
+    //  since we are working with both an object and an array, i started by pulling the keys from the bosses object, which returns an array. i then iterated over that array with map in order to create another array of the same length, and used each key to access the bosses object by bracket notation and get the name of the boss. i iterated over that new array of boss names using map (again, to create an array of the same length) and created a new object which would be the elements of my array. inside of the boss object i defined keys of the boss name and their sidekick loyalty, the last of which i set to zero as a starting value. i then iterated over the sidekicks array in the dataset with a forEach and had it add the loyalty of the boss object sidekickLoyalty property. 
   }
 };
 
